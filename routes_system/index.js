c968499235dd0514960a7a979c753df1a2f754f2
Vitaly Karich <kantotemus@gmail.com>
2026-02-02 00:55:16 -0800

    fix: wire system payments webhook route

--- routes_system/index.js
+++ routes_system/index.js
@@ -1,8 +1,15 @@
+// routes_system/index.js
 import express from "express";
+
 import bookingTimeout from "./bookingTimeout.js";
+import paymentsWebhook from "./paymentsWebhook.js";
 
 const router = express.Router();
 
+// system jobs
 router.use("/", bookingTimeout);
+
+// payments webhook
+router.use("/payments", paymentsWebhook);
 
 export default router;
